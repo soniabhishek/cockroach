a2a596e199045be3974057b825d4e1dde298049e
himanshu144141 <himanshu144141@gmail.com>
2016-06-05 12:41:58 +0530

    added buffer in step

--- app/services/work_flow_svc/step/step.go
+++ app/services/work_flow_svc/step/step.go
@@ -1,7 +1,9 @@
 package step
 
 import (
+	"errors"
 	"gitlab.com/playment-main/angel/app/models"
+	"gitlab.com/playment-main/angel/app/models/uuid"
 	"gitlab.com/playment-main/angel/app/services/work_flow_svc/feed_line"
 )
 
@@ -14,23 +16,29 @@ type Step struct {
 
 func New() Step {
 	return Step{
-		InQ:  feed_line.New(),
-		OutQ: feed_line.New(),
+		InQ:    feed_line.New(),
+		OutQ:   feed_line.New(),
+		buffer: feed_line.NewBuffer(),
 	}
 }
 
 func (s *Step) AddToBuffer(flu feed_line.FLU) {
-	s.buffer.Add(flu)
+	s.buffer.Save(flu)
 }
 
-func (s *Step) GetBuffered() feed_line.Bf {
+func (s *Step) GetBuffered() map[uuid.UUID]feed_line.FLU {
 
-	return s.buffer
+	return s.buffer.GetAll()
 }
 
 func (s *Step) RemoveFromBuffer(flu feed_line.FLU) error {
 
+	flu, ok := s.buffer.Get(flu.ID)
+	if !ok {
+		return errors.New("not present")
+	}
 	return nil
+
 }
 
 func (s *Step) Detain(flu feed_line.FLU, why error, saver iFluSave) {

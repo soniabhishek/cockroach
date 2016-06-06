6d47be17df92f41d4db17a3ee19a84479a78a91c
himanshu144141 <himanshu144141@gmail.com>
2016-06-06 11:15:59 +0530

    added route getter

---
+++ app/services/work_flow_svc/step_router/starter.go
@@ -0,0 +1,56 @@
+package step_router
+
+import (
+	"errors"
+
+	"gitlab.com/playment-main/angel/app/models/step_type"
+	"gitlab.com/playment-main/angel/app/plog"
+	"gitlab.com/playment-main/angel/app/services/work_flow_svc/counter"
+)
+
+func start(sr *stepRouter) {
+
+	// Start listening for incoming flus from InQ channel
+	// in another goroutine & route it to its exact step
+	go func() {
+
+		for {
+			select {
+			case flu := <-sr.InQ:
+
+				// There is a question that adding to the
+				// buffer should be inside or outside
+				// the below go routine.
+				//
+				// Current implementation causes InQ to block if channel is full.
+				// If put inside the below go routine InQ will become non blocking
+				// but there is a chance of large number of go routines at a time
+				sr.buffer <- 1
+
+				go func() {
+
+					defer func() {
+
+						if r := recover(); r != nil {
+							plog.Error("Router", errors.New("Panic occured in router"), r)
+							*sr.routeTable[step_type.Manual] <- flu
+							plog.Info("Router", "Sent to manual after panic", flu)
+						}
+
+						// Free the buffer
+						<-sr.buffer
+					}()
+
+					// Add workers here
+					// Right now its like just one sync worker
+					// i.e. if the below method takes 1 second & buffer size is 10
+					// then the max speed of router processing will be 1 flu * buffer/second = 10 flu/second
+					r := sr.getRoute(&flu)
+					counter.Print(flu, "router")
+					*r <- flu
+				}()
+
+			}
+		}
+	}()
+}

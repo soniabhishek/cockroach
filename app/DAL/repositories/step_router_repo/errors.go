c7cdb0cfb94cc53329a887b0e28e520de603c3f6
himanshu144141 <himanshu144141@gmail.com>
2016-06-06 11:19:47 +0530

    step router repo

--- app/DAL/repositories/step_router_repo/errors.go
+++ app/DAL/repositories/step_router_repo/errors.go
@@ -2,4 +2,4 @@ package step_router_repo
 
 import "gitlab.com/playment-main/angel/app/services/plerrors"
 
-var ErrRouteNotFound = plerrors.ServiceError{}
+var ErrRouteNotFound = plerrors.ServiceError{"SR_0001", "Route not found"}

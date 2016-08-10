af33898cfbf4ce8229fce4bb2ea88b06406be260
himanshu144141 <himanshu144141@gmail.com>
2016-08-10 18:54:45 +0530

    Signed-off-by: himanshu144141 <himanshu144141@gmail.com>

--- app/DAL/repositories/feed_line_repo/extra.go
+++ app/DAL/repositories/feed_line_repo/extra.go
@@ -64,23 +64,90 @@ func SyncAll() {
 	}
 
 	plog.Info("extra", len(flusToInsert), " new flus inserted")
+}
 
-	/*
+func SyncAllBack() {
+
+	inpQ := NewInputQueue()
+
+	existingQFlus := []feedLineInputModel{}
+
+	err := inpQ.mgo.C("feedline_input").Find(bson.M{}).All(&existingQFlus)
+	if err != nil {
+		plog.Error("feedline", err)
+		return
+	}
+
+	plog.Info("existingQFlus", len(existingQFlus))
+
+	fluRepo := fluRepo{
+		Db: postgres.GetPostgresClient(),
+	}
+
+	flus := []models.FeedLineUnit{}
+
+	_, err = fluRepo.Db.Select(&flus, "SELECT * FROM feed_line")
+	if err != nil {
+		plog.Error("feedline", err)
+		return
+	}
+
+	plog.Info("flus", len(flus))
+
+	flusToUpdate := []feedLineInputModel{}
+	flusToInsert := []interface{}{}
+
+	for _, eflu := range flus {
+
+		if f, ok := existsInListInQ(existingQFlus, eflu.ID); ok {
+
+			if f.Status == queued {
+
+				flusToUpdate = append(flusToUpdate, *f)
+			}
+		} else {
+
+			//flusToInsert = append(flusToInsert, )
+		}
+	}
+
+	err = fluRepo.Db.Insert(flusToInsert...)
+	if err != nil {
+		plog.Error("feedline", err)
+		return
+	}
+
+	plog.Info("extra", len(flusToInsert), " new flus inserted")
+
+	if len(flusToInsert) > 0 {
 		err = inpQ.mgo.C("feedline_input").Insert(flusToInsert...)
 		if err != nil {
 			plog.Error("feedline", err)
 		}
 
-		for _, updateFlu := range flusToUpdate {
+	}
 
-			err = inpQ.mgo.C("feedline_input").UpdateId(updateFlu.ID, updateFlu)
-			if err != nil {
-				plog.Error("feedline", err, updateFlu)
-			}
-		}*/
+	for _, updateFlu := range flusToUpdate {
+
+		err = inpQ.mgo.C("feedline_input").UpdateId(updateFlu.ID, updateFlu)
+		if err != nil {
+			plog.Error("feedline", err, updateFlu)
+		}
+	}
 }
 
 func existsInList(list []models.FeedLineUnit, toFindId uuid.UUID) (*models.FeedLineUnit, bool) {
+	for _, elem := range list {
+		if elem.ID == toFindId {
+
+			plog.Info("asd", elem.ID)
+			return &elem, true
+		}
+	}
+	return nil, false
+}
+
+func existsInListInQ(list []feedLineInputModel, toFindId uuid.UUID) (*feedLineInputModel, bool) {
 	for _, elem := range list {
 		if elem.ID == toFindId {
 

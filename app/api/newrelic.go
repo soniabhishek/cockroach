22941bb33c191a89537a2021eb64f9789bfee0c1
Himanshu <himanshu144141@gmail.com>
2016-08-18 13:53:14 +0530

    getting api handler name

--- app/api/newrelic.go
+++ app/api/newrelic.go
@@ -3,12 +3,16 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/newrelic/go-agent"
+	"strings"
 )
 
 func NewRelicMiddleware(app newrelic.Application) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
-		txn := app.StartTransaction(c.HandlerName(), c.Writer, c.Request)
+		handlerStrings := strings.Split(c.HandlerName(), "/")
+		handlerName := handlerStrings[len(handlerStrings)-1]
+
+		txn := app.StartTransaction(handlerName, c.Writer, c.Request)
 		defer txn.End()
 
 		c.Next()

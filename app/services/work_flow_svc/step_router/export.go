4fdf0dd96db2eb5fe72e3db3ae5be392310d35ce
Himanshu <himanshu144141@gmail.com>
2016-09-23 16:55:24 +0530

    increasing step router concurrency

--- app/services/work_flow_svc/step_router/export.go
+++ app/services/work_flow_svc/step_router/export.go
@@ -2,7 +2,7 @@ package step_router
 
 func newStdStepRouter() stepRouter {
 
-	router := newStepRouter(10)
+	router := newStepRouter(50)
 
 	// Connect to all the steps (in this case steps like crowd sourcing)
 	router.connectAll()

de9aed1c05981e959d942f209d32a9e894ab15ad
Himanshu <himanshu144141@gmail.com>
2016-10-13 08:37:32 +0530

    flu_output_force

---
+++ app/services/flu_svc/flu_output/flu_output_force.go
@@ -0,0 +1,121 @@
+package flu_output
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/crowdflux/angel/app/DAL/repositories/feed_line_repo"
+	"github.com/crowdflux/angel/app/DAL/repositories/project_configuration_repo"
+	"github.com/crowdflux/angel/app/models"
+	"github.com/crowdflux/angel/app/models/status_codes"
+	"github.com/crowdflux/angel/app/models/uuid"
+	"github.com/crowdflux/angel/app/plog"
+	"github.com/crowdflux/angel/utilities"
+	"net/http"
+	"time"
+)
+
+func ForceSendBack(stepId uuid.UUID, projectId uuid.UUID, concurrency int) {
+
+	config, err := project_configuration_repo.New().Get(projectId)
+	if err != nil {
+		panic(err)
+	}
+
+	flus, err := feed_line_repo.New().GetFlusNotSent(stepId)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("starting for flus: ", len(flus))
+
+	var channelSize = concurrency
+
+	c := make(chan int, channelSize)
+
+	start := time.Now()
+
+	for i := 0; i < len(flus)+channelSize; i++ {
+
+		if i < len(flus) {
+			go sendFluBack(config, flus[i], c)
+		}
+		if i >= channelSize {
+			<-c
+			fmt.Println(i-channelSize, time.Since(start))
+		}
+	}
+}
+
+func sendFluBack(config models.ProjectConfiguration, flu models.FeedLineUnit, chn chan int) {
+	defer func() {
+		chn <- 1
+	}()
+	fluOutputStrct := fluOutputStruct{
+		ID:          flu.ID,
+		ReferenceId: flu.ReferenceId,
+		Tag:         flu.Tag,
+		Status:      STATUS_OK,
+		Result:      flu.Build[RESULT],
+	}
+
+	fluResp, status := sendBackToClientCustom(config, []fluOutputStruct{fluOutputStrct})
+	if status == status_codes.Success {
+
+		putDbLog([]models.FeedLineUnit{flu}, SUCCESS, *fluResp)
+
+	} else if status == status_codes.CallBackFailure {
+
+		fmt.Println("failure", fluResp)
+
+		//not successful scenarios
+		//putDbLog([]models.FeedLineUnit{flu}, "ERROR", *fluResp)
+	} else {
+		fmt.Println("failure", fluResp)
+
+		//putDbLog([]models.FeedLineUnit{flu}, "ERROR", *fluResp)
+	}
+}
+
+func sendBackToClientCustom(fpsModel models.ProjectConfiguration, fluProjectResp []fluOutputStruct) (*Response, status_codes.StatusCode) {
+
+	if len(fluProjectResp) < 1 {
+		return &Response{}, status_codes.NoFluToSend
+	}
+
+	url := fpsModel.PostBackUrl
+	//url := "http://localhost:8080/JServer/HelloServlet"
+	plog.Trace("URL:>", url, "|ID: ", fpsModel.ProjectId, "|Body:", fluProjectResp)
+
+	sendResp := make(map[string][]fluOutputStruct)
+	sendResp["feed_line_units"] = fluProjectResp
+	jsonBytes, err := json.Marshal(sendResp)
+	if err != nil {
+		plog.Error("JSON Marshalling Error:", err)
+		return &Response{}, status_codes.UnknownFailure
+	}
+	jsonBytes = utilities.ReplaceEscapeCharacters(jsonBytes)
+	plog.Trace("Sending JSON:", string(jsonBytes))
+
+	//fmt.Println(hex.EncodeToString(sig.Sum(nil)))
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBytes))
+	req.Header.Set(CONTENT_TYPE, TYPE_JSON)
+
+	for headerKey, headerVal := range fpsModel.Headers {
+		req.Header.Set(headerKey, headerVal.(string))
+
+	}
+	addSendBackAuth(req, fpsModel, jsonBytes)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		plog.Error("HTTP Error:", err)
+		return &Response{}, status_codes.UnknownFailure
+	}
+
+	fluResp, status := validationErrorCallback(resp)
+	fluResp.FluStatusCode = status
+	return fluResp, status
+}
--- experiments/playground.go
+++ experiments/playground.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"github.com/crowdflux/angel/app/DAL/repositories/feed_line_repo"
+	"github.com/crowdflux/angel/app/models/uuid"
+	"github.com/crowdflux/angel/app/services/flu_svc/flu_output"
 )
 
 func main() {
 
-	feed_line_repo.SyncAllBack()
+	stepId := uuid.FromStringOrNil("43dacf34-83fc-4628-b276-23f05d23e6f7")
+	projectId := uuid.FromStringOrNil("dff44216-f42e-484d-aade-8004ccb1bf79")
+
+	flu_output.ForceSendBack(stepId, projectId, 1)
 
 }

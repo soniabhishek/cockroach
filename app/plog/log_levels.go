f2f592ab299c8ab46cdeb9b7ef11eb0865d67289
Abhishek Soni <abhishek@Abhisheks-MacBook-Pro.local>
2016-10-25 13:16:21 +0530

    spacing issue

--- app/plog/log_levels.go
+++ app/plog/log_levels.go
@@ -1,17 +1,17 @@
 package plog
 
 import (
-	"runtime"
-	"github.com/Sirupsen/logrus"
 	"fmt"
+	"github.com/Sirupsen/logrus"
+	"runtime"
 )
 
 func Fatal(tag string, err error, args ...interface{}) {
 	if levelFatal >= plogLevel {
 		ErrorMail(tag, err, args)
 		logr.WithFields(logrus.Fields{
-			"error" : err,
-			"args"  : fmt.Sprintf("%+v , ",args),
+			"error": err,
+			"args":  fmt.Sprintf("%+v , ", args),
 		}).Fatalln(tag)
 	}
 }
@@ -19,8 +19,8 @@ func Fatal(tag string, err error, args ...interface{}) {
 func Error(tag string, err error, args ...interface{}) {
 	if levelError >= plogLevel {
 		logr.WithFields(logrus.Fields{
-			"error" : err,
-			"args"  : fmt.Sprintf("%+v",args),
+			"error": err,
+			"args":  fmt.Sprintf("%+v", args),
 		}).Errorln(tag)
 		ErrorMail(tag, err, args)
 	}
@@ -29,7 +29,7 @@ func Error(tag string, err error, args ...interface{}) {
 func Warn(tag string, args ...interface{}) {
 	if levelWarn >= plogLevel {
 		logr.WithFields(logrus.Fields{
-			"args" : fmt.Sprintf("%+v",args),
+			"args": fmt.Sprintf("%+v", args),
 		}).Warnln(tag)
 	}
 }
@@ -37,20 +37,19 @@ func Warn(tag string, args ...interface{}) {
 func Info(tag string, args ...interface{}) {
 	if levelInfo >= plogLevel {
 		logr.WithFields(logrus.Fields{
-			"args": fmt.Sprintf("%+v",args),
+			"args": fmt.Sprintf("%+v", args),
 		}).Infoln(tag)
 	}
 }
 
-
 func Debug(tag string, args ...interface{}) {
 
-	if levelDebug >= plogLevel{
+	if levelDebug >= plogLevel {
 		_, fn, line, _ := runtime.Caller(1)
 		logr.WithFields(logrus.Fields{
-			"file" : fn,
-			"line" : line,
-			"args" : fmt.Sprintf("%+v",args),
+			"file": fn,
+			"line": line,
+			"args": fmt.Sprintf("%+v", args),
 		}).Debugln(tag)
 	}
 }
@@ -61,9 +60,9 @@ func Trace(tag string, args ...interface{}) {
 		_, fn, line, _ := runtime.Caller(1)
 		logr.WithFields(logrus.Fields{
 			"level": "trace",
-			"file" : fn,
-			"line" : line,
-			"args" : fmt.Sprintf("%+v",args),
+			"file":  fn,
+			"line":  line,
+			"args":  fmt.Sprintf("%+v", args),
 		}).Debugln(tag)
 	}
 }

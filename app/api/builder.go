984cf3864fa63986f2446438d84b1348611da5ea
himanshu144141 <himanshu144141@gmail.com>
2016-10-25 23:38:13 +0530

    refactoring

--- app/api/builder.go
+++ app/api/builder.go
@@ -1,25 +1,23 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/crowdflux/angel/app/api/auther"
 	"github.com/crowdflux/angel/app/api/handlers"
 	"github.com/crowdflux/angel/app/config"
 	"github.com/crowdflux/angel/app/services/flu_svc/flu_svc_transport"
-	"github.com/crowdflux/angel/app/services/image_svc1"
 	"github.com/crowdflux/angel/utilities/clients/api"
 	"github.com/gin-gonic/gin"
 
 	"time"
 
 	"github.com/crowdflux/angel/app/services/work_flow_builder_svc"
+	"github.com/crowdflux/angel/app/services/work_flow_retriever_svc"
 	"github.com/crowdflux/angel/app/services/work_flow_svc/step/crowdsourcing_step_svc"
 	"github.com/crowdflux/angel/app/services/work_flow_svc/step/manual_step_svc"
 	"github.com/itsjamie/gin-cors"
 	"github.com/newrelic/go-agent"
-	"github.com/crowdflux/angel/app/services/work_flow_retriever_svc"
 )
 
 func Build() {
@@ -60,11 +58,9 @@ func Build() {
 		r.Use(NewRelicMiddleware(newrelicApp))
 	}
 
-	fmt.Println(config.DOWNLOAD_PATH.Get())
 	r.StaticFS("/downloadedfiles", http.Dir(config.DOWNLOAD_PATH.Get()))
 
 	//Api prefix
-
 	api := r.Group("/api/v0")
 	{
 
@@ -74,6 +70,8 @@ func Build() {
 		crowdsourcing_step_svc.AddHttpTransport(api)
 		manual_step_svc.AddHttpTransport(api)
 		utils_api.AddHttpTransport(api)
+		work_flow_retriever_svc.AddHttpTransport(api)
+		work_flow_builder_svc.AddHttpTransport(api)
 	}
 
 	authorized := r.Group("/api/v0", auther.GinAuther())
@@ -81,17 +79,5 @@ func Build() {
 		flu_svc_transport.AddHttpTransport(authorized)
 	}
 
-	workFlow := r.Group("/api/v0")
-	{
-		work_flow_builder_svc.AddHttpTransport(workFlow)
-	}
-
-	workFlowRetriever := r.Group("/api/v0")
-	{
-		work_flow_retriever_svc.AddHttpTransport(workFlowRetriever)
-	}
-	var _ image_svc1.IImageService
-
 	r.Run(":8999") // listen and serve on 127.0.0.1:8999
-
 }

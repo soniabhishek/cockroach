a1fd8196a0b00752d46f90d4666987700afadb96
Himanshu <himanshu144141@gmail.com>
2016-11-22 09:48:19 +0530

    fixing migrations

--- app/DAL/migrations/20161107073541_separate_tags_table.js
+++ app/DAL/migrations/20161107073541_separate_tags_table.js
@@ -1,7 +1,9 @@
+import co from 'co'
 
 exports.up = function(knex, Promise) {
-  return Promise.all([
-  knex.schema
+  return co(function*(){
+
+    yield knex.schema
     .createTable('work_flow_tag_associators',t=>{
             t.increments()
             t.string('tag_name')
@@ -10,18 +12,21 @@ exports.up = function(knex, Promise) {
                 .notNullable()
                 .index()
             t.uuid('project_id')
-				.notNullable()
+        .notNullable()
             t.timestamp('created_at')
             t.unique(['tag_name','project_id'])
             t.foreign('work_flow_id').references('work_flow.id')
             t.foreign('project_id').references('projects.id')
-    }),
-   knex.schema
+    })
+    yield knex.raw(`
+      INSERT INTO work_flow_tag_associators (tag_name, work_flow_id, project_id, created_at)
+      SELECT tag,id,project_id,now() FROM work_flow;`)
+    yield knex.schema
     .table('work_flow', t=> {
-    t.dropColumn('tag')
+      t.dropColumn('tag')
     })
 
-  ])
+  })
 };
 
 exports.down = function(knex, Promise) {

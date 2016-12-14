39fea26242ed9c006d27ac542a017127fb6d5700
Himanshu <himanshu144141@gmail.com>
2016-12-14 18:12:20 +0530

    removing junk code

--- app/services/work_flow_executor_svc/step/unification_step_svc/unification_step.go
+++ app/services/work_flow_executor_svc/step/unification_step_svc/unification_step.go
@@ -21,16 +21,6 @@ type unificationStep struct {
 
 func (u *unificationStep) processFlu(flu feed_line.FLU) {
 
-	uuidsToSkip := []uuid.UUID{uuid.FromStringOrNil("179700d8-7ddd-48a9-b0e5-98c83b7eb144"), uuid.FromStringOrNil("6b278451-d350-4285-9ba9-849cb9a5bf2b"), uuid.FromStringOrNil("b9f71d53-fd8e-4acc-a980-d8f0a2b35667"), uuid.FromStringOrNil("4f494114-dbea-4c4f-a0ae-b29e7cd3ecad"), uuid.FromStringOrNil("15817696-c129-430f-a2bc-f7996fc861da"), uuid.FromStringOrNil("ac5d5b68-8b4b-4c76-9b21-ffa594d8dc16"), uuid.FromStringOrNil("5aeb0e8d-0e9a-43a6-a7e6-d4a86d4a7084"), uuid.FromStringOrNil("7db091b2-cac9-4d70-b086-137ab4b92255"), uuid.FromStringOrNil("593c1eb2-0a22-4ce8-b0b7-43a289efa6c8"), uuid.FromStringOrNil("ed5bac16-7824-4c90-b398-ac546e48a608"), uuid.FromStringOrNil("9db31b80-ca0e-469f-8904-9b82be606935"), uuid.FromStringOrNil("21a834af-289a-46af-80c1-9eff5617236a"), uuid.FromStringOrNil("b198817c-9118-46fe-9cb9-05ee632d4035"), uuid.FromStringOrNil("bace65ec-28a2-4606-bd20-658be604006d"), uuid.FromStringOrNil("0bd8dfb1-b6c8-4e42-a0d6-38ff0eff0750"), uuid.FromStringOrNil("1842c3a1-49e7-47d0-822c-d3ad9ce7637f")}
-	for _, skipId := range uuidsToSkip {
-
-		if skipId == flu.ID {
-			fmt.Println("unification skipped", "fluid: "+flu.ID.String())
-			flu.ConfirmReceive()
-			return
-		}
-	}
-
 	unificationConfig, err := u.stepConfigSvc.GetUnificationStepConfig(flu.StepId)
 	if err != nil {
 		plog.Error("Unification Step", err, "error getting step", "fluId: "+flu.ID.String())

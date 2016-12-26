b8cd45607f0af2138b271e13f73eadf6f3457108
Himanshu <himanshu144141@gmail.com>
2016-12-26 15:19:39 +0530

    skipping flus from unification step

---
+++ app/services/work_flow_executor_svc/step/skip.go
@@ -0,0 +1,36 @@
+package step
+
+import (
+	"github.com/crowdflux/angel/app/models/uuid"
+	"github.com/crowdflux/angel/app/plog"
+)
+
+func getFluIdFromString (fluIdsString []string) []uuid.UUID {
+
+	fluIds := make([]uuid.UUID, len(fluIdsString))
+
+	for i, fluIdString := range fluIdsString {
+
+		fluIds[i] = uuid.FromStringOrNil(fluIdString)
+	}
+	return fluIds
+}
+
+var FluIDsToSkip = []uuid.UUID{}
+
+func init() {
+	FluIDsToSkip = getFluIdFromString(fluIDToSkipString)
+}
+
+
+func IsSkipped( fluID uuid.UUID ) bool {
+
+	for _, fluIDToSkip := range FluIDsToSkip {
+
+		if fluID == fluIDToSkip {
+			plog.Info("skipping flus", "fluId: "+fluID.String())
+			return true
+		}
+	}
+	return false
+}

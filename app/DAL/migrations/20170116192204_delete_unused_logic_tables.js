7cc318a1f5bd63e1f99eacc80688cf2ab33a07bc
Govinda Murali <govinda.murali009@gmail.com>
2017-01-17 17:57:48 +0530

    Adding test condition

--- app/DAL/migrations/20170116192204_delete_unused_logic_tables.js
+++ app/DAL/migrations/20170116192204_delete_unused_logic_tables.js
@@ -10,11 +10,12 @@ exports.up = function(knex, Promise) {
             .dropTableIfExists('logic_gate'),
         knex
             .schema
-            .dropTableIfExists('logic_gate_formula')
+            .dropTableIfExists('logic_gate_formula'),
+        knex
+            .raw(`update routes set config = concat('{"input_template":[',r2.config, ']}')::jsonb from routes as r1 join routes as r2 on r1.id = r2.id`)
 
     ])
 };
 
 exports.down = function(knex, Promise) {
-  
 };
--- app/services/work_flow_executor_svc/router_svc/router_getter_test.go
+++ app/services/work_flow_executor_svc/router_svc/router_getter_test.go
@@ -10,9 +10,14 @@ import (
 
 func TestRouter_Config(t *testing.T) {
 
-	flu := feed_line.FLU{FeedLineUnit: models.FeedLineUnit{ID: uuid.FromStringOrNil("eaf3b794-40c1-4132-a1b0-3e6a81c939ae"), StepId: uuid.FromStringOrNil("662cda89-ce3f-40f0-ae54-0c8dff082502"), Data: models.JsonF{"rc_check_res_success": true}, Build: models.JsonF{"rc_check_res_success": true}}}
+	flu := feed_line.FLU{FeedLineUnit: models.FeedLineUnit{ID: uuid.NewV4(), StepId: uuid.FromStringOrNil("662cda89-ce3f-40f0-ae54-0c8dff082502"), Data: models.JsonF{"rc_check_res_success": true}, Build: models.JsonF{"rc_check_res_success": true}}}
+	flu2 := feed_line.FLU{FeedLineUnit: models.FeedLineUnit{ID: uuid.NewV4(), StepId: uuid.FromStringOrNil("662cda89-ce3f-40f0-ae54-0c8dff082502"), Data: models.JsonF{"rc_check_res_success": true}, Build: models.JsonF{"rc_check_res_success": false}}}
+
 	routerGetter := newRouteGetter()
 	s, err := routerGetter.GetNextStep(flu)
+	s2, err2 := routerGetter.GetNextStep(flu2)
 	assert.NoError(t, err)
+	assert.NoError(t, err2)
 	assert.ObjectsAreEqual(s, uuid.FromStringOrNil("a3960044-d575-4b73-b208-c2764cee2062"))
+	assert.ObjectsAreEqual(s2, uuid.FromStringOrNil("93367a4b-bdb4-4a00-803b-ca52b5fb6c7b"))
 }

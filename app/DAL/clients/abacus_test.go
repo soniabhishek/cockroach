98b11dd8a01a2c0202c25ba5832d43ffd226a36c
Govinda Murali <govinda.murali009@gmail.com>
2017-01-19 21:16:57 +0530

    Refactoring old test cases for algorithm step

--- app/DAL/clients/abacus_test.go
+++ app/DAL/clients/abacus_test.go
@@ -1,24 +1,43 @@
 package clients
 
 import (
+	"encoding/json"
+	"github.com/crowdflux/angel/app/config"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/jarcoal/httpmock.v1"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
-// WORKS ONLY 10% OF THE TIME DUE TO 10% SUCCESS LOGIC IN ABACUS
-func TestAbacusSuccesfulPrediction(t *testing.T) {
+func TestAbacusPrediction(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	var request algorithmRequest
+	var err error
+	httpmock.RegisterResponder("POST", config.ABACUS_API.Get()+"/api/review", func(req *http.Request) (*http.Response, error) {
+		body, _ := ioutil.ReadAll(req.Body)
+		err = json.Unmarshal(body, &request)
+		if request.Input == "Good item" {
+			return httpmock.NewStringResponse(200, `{"prediction":"Approve", "success":true}`), nil
+		} else {
+			return httpmock.NewStringResponse(200, `{ "success":false}`), nil
+		}
+	},
+	)
+
 	cc := GetAbacusClient()
-	actualResult, err, success := cc.Predict("Good item")
-	expectedResult := "Approve-Useful Product Review"
-	assert.Equal(t, expectedResult, actualResult)
+
+	actualGoodResult, err, success := cc.Predict("Good item")
+	expectedGoodResult := "Approve"
+
+	assert.Equal(t, expectedGoodResult, actualGoodResult)
 	assert.True(t, success)
 	assert.NoError(t, err)
-}
 
-func TestAbacusUnsuccessfulPrediciton(t *testing.T) {
-	cc := GetAbacusClient()
-	actualResult, err, success := cc.Predict("Jango Fett")
-	assert.NotEqual(t, actualResult, "Approve")
+	actualBadResult, err, success := cc.Predict("Jango Fett")
+	assert.NotEqual(t, actualBadResult, "Approve")
 	assert.False(t, success)
 	assert.NoError(t, err)
 }

1ac7b88afda564fbdee530641e507fdcdd390430
Govinda Murali <govinda.murali009@gmail.com>
2017-01-20 15:02:43 +0530

    migrations

--- app/DAL/migrations/20170116192204_delete_unused_logic_tables.js
+++ app/DAL/migrations/20170116192204_delete_unused_logic_tables.js
@@ -1,21 +1,43 @@
+import co from 'co'
 
 exports.up = function(knex, Promise) {
-    return Promise.all([
-        knex
-            .schema
-            .table('routes', t=> {
-            t.dropColumn('logic_gate_id')}),
-        knex
-            .schema
-            .dropTableIfExists('logic_gate'),
-        knex
-            .schema
-            .dropTableIfExists('logic_gate_formula'),
-        knex
-            .raw(`update routes set config = concat('{"input_template":[',r2.config, ']}')::jsonb from routes as r1 join routes as r2 on r1.id = r2.id`)
-
-    ])
+    return co(function * () {
+        yield knex.schema.table('routes', t=> {t.dropColumn('logic_gate_id')})
+        yield knex.schema.dropTableIfExists('logic_gate')
+        yield knex.schema.dropTableIfExists('logic_gate_formula')
+        yield knex.raw(`update routes set config = concat('{"input_template":[',r2.config, ']}')::jsonb from routes as r1 join routes as r2 on r1.id = r2.id`)
+    })
 };
 
+
+
 exports.down = function(knex, Promise) {
+    return co(function * () {
+
+        yield knex.schema
+                .createTable('logic_gate_formula',t => {
+                    t.integer('id')
+                    .primary()
+                t.string('name')
+        })
+
+        yield knex.schema
+        	    .createTable('logic_gate',t=>{
+                    t.uuid('id')
+                    .primary()
+                t.specificType('input_template','jsonb')
+                t.integer('formula')
+                    .notNullable()
+                    .references('id')
+                    .inTable('logic_gate_formula')
+        	    })
+
+        yield knex.schema
+                .table('routes', t => {
+                    t.uuid('logic_gate_id')
+                    .references('id')
+                    .inTable('logic_gate')
+                    .onDelete('CASCADE')
+                })
+    })
 };
